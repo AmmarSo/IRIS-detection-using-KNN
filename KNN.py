d02b377ac5f96c906c66e6febbcaf9d19f04b9d4
Killam94 <99586566+Killam94@users.noreply.github.com>
2025-04-21 10:22:46 +0200

    Final

--- KNN.py
+++ KNN.py
@@ -2,10 +2,11 @@
 import pandas as pd
 import math
 
-# Load the dataset
+# Load the dataset and variable initialization
 dataframe = pd.read_csv("data/iris.csv")
 distance = {}
 iris = []
+iris_class = {'Iris-setosa':0, 'Iris-versicolor':0, 'Iris-virginica':0 }
 
 # Euclidean distance function that skips the first element (the ID)
 def euclidean_distance(x, xn):
@@ -16,7 +17,7 @@ def euclidean_distance(x, xn):
     return math.sqrt(dist)
 
 # New point (first element is its ID)
-xn = [151, 5.5, 2.4, 0.2]
+xn = [151, 6.0,  2.8,  4.9,  1.7]
 
 # Compute distance from each iris to the new point
 for i in range(len(dataframe)):
@@ -29,13 +30,28 @@ for i in range(len(dataframe)):
     iris = [id_i, d1, d2, d3, d4]
     distance_with_xnew = euclidean_distance(iris, xn)
     distance[id_i] = distance_with_xnew
-    #print(f"ID {id_i} → Distance = {distance_with_xnew}")
+    print(f"ID {id_i} → Distance = {distance_with_xnew}")
 
 
-k = 3
+k = 15
+
 # Sort in ascending order
 sorted_items = sorted(distance.items(), key=lambda item: item[1])
-
 # We choose the k smallest element 
 k_smallest = sorted_items[:k]
-print(k_smallest)
+#print(k_smallest)
+
+# Retrieve class 
+for item in k_smallest:
+    id = item[0]
+    filtered_df = dataframe[dataframe['Id'] == id]
+    y = filtered_df["Species"].to_list()
+    if y[0] == "Iris-setosa":
+        iris_class["Iris-setosa"] = iris_class["Iris-setosa"] + 1
+    elif y[0] == "Iris-versicolor":
+        iris_class["Iris-versicolor"] = iris_class["Iris-versicolor"] + 1
+    else:
+        iris_class["Iris-virginica"] = iris_class["Iris-virginica"] + 1
+        
+print("Vote :", iris_class)
+print("Class of Pnew :", max(iris_class, key=iris_class.get))
